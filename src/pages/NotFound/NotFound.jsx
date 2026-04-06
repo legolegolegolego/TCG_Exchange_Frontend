cbee4d1ca532add9b5b2a08877366885949e71c1
L <srleon57@gmail.com>
2026-04-06 12:45:05 +0200

    arreglo imagen 404

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,16 +1,14 @@
-import { useNavigate } from "react-router-dom";
 import styles from "./NotFound.module.css";
-import missingno from "../../assets/missingno.webp";
+import missingno from "../../assets/missingno.png";
 
 const NotFound = () => {
-  const navigate = useNavigate();
 
   return (
     <div className={`container ${styles.wrapper} text-center d-flex flex-column justify-content-center align-items-center`}>
       <img
         src={missingno}
         alt="MissingNo"
-        className={`img-fluid ${styles.image}`}
+        className={`${styles.image}`}
       />
 
       <h1 className={`display-1 ${styles.code}`}>404</h1>
