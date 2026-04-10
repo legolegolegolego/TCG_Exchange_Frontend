bfa973ffd4f56d53da951cfe14c8406ae8ade1d4
L <srleon57@gmail.com>
2026-04-10 16:40:27 +0200

    url backend render

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080",
+  // baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080",
+  baseURL: import.meta.env.VITE_API_URL || "https://tcg-exchange.onrender.com",
 });
 
 // Interceptor para agregar token a todas las peticiones que lo requieran
